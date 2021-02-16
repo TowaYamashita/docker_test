6a2503f2428ddd0dbc2cdd4d1d539f764cd841df
山下永遠 <yamashita@laptop-8rine23.local>
2021-02-16 11:05:14 +0900

    modify UI

--- volumes/web/sample/template/edit.php
+++ volumes/web/sample/template/edit.php
@@ -5,26 +5,42 @@
         <?= $title ?>
     </h1>
 
-    <form action="index.php" method="post">
-        <input type="hidden" name="mode" value="update">
-        <input type="hidden" name="id" value="<?= $todoList['id'] ?>">
-        <div class="form-group  mt-md-3">
-            <label for="title">
-                Title(Within 140 characters)
-            </label>
-            <input type="text" class="form-control" id="title" name="title" max="140" value="<?php echo $todoList['title'] ?>" required>
+    <div class="card border-primary">
+        <div class="card-header">
+            TODOの編集
         </div>
-        <div class="form-group  mt-md-3">
-            <label for="deadline">
-                Deadline
-            </label>
-            <input type="datetime-local" id="deadline" name="deadline" step="600" value="<?php echo str_replace(" ", "T", $todoList['finished_at']) ?>" equired>
-        </div>
-        <button type="submit" class="btn btn-primary">
-            更新
-        </button>
-    </form>
-
+        <div class="card-body">
+            <form action="index.php" method="post">
+            <input type="hidden" name="mode" value="update">
+            <input type="hidden" name="id" value="<?= $todoList['id'] ?>">
+            <div class="form-group row">
+                <label for="title" class="col-sm-4 col-form-label">
+                    やること(140文字以内)
+                </label>
+                <div class="col-sm-8">
+                    <input type="textarea" class="form-control" id="title" name="title" max="140" value="<?php echo $todoList['title'] ?>" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="deadline" class="col-sm-4 col-form-label">
+                    期限
+                </label>
+                <div class="col-sm-8">
+                    <input type="datetime-local" id="deadline" name="deadline" step="600" value="<?php echo str_replace(" ", "T", $todoList['finished_at']) ?>" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-10">
+                    <button type="submit" class="btn btn-primary">
+                        更新
+                    </button>
+                    <button type="button" class="btn btn-outline-secondary" onclick="location.href='index.php'">
+                        戻る
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
 
 <?= $script ?>
--- volumes/web/sample/template/index.php
+++ volumes/web/sample/template/index.php
@@ -29,13 +29,13 @@
                     </h5>
                     <div>
                         <?php if(!$todo['finished']): ?>
-                            <button type="button" class="btn btn-success code-edit-todo" value="<?= $todo['id'] ?>">
+                            <button type="button" class="btn btn-info code-edit-todo" value="<?= $todo['id'] ?>">
                                 編集
                             </button>
-                            <button type="button" class="btn btn-danger code-delete-todo" value="<?= $todo['id'] ?>">
+                            <button type="button" class="btn btn-outline-danger code-delete-todo" value="<?= $todo['id'] ?>">
                                 削除
                             </button>
-                            <button type="button" class="btn btn-secondary code-finish-todo" value="<?= $todo['id'] ?>">
+                            <button type="button" class="btn btn-success code-finish-todo" value="<?= $todo['id'] ?>">
                                 完了
                             </button>
                         <?php endif;?>
@@ -44,26 +44,41 @@
             </li>
         <?php endforeach; ?>
     </ul>
+</div>
 
-    <form action="index.php" method="post">
-        <input type="hidden" name="mode" value="create">
-        <div class="form-group  mt-md-3">
-            <label for="title">
-                TODO(Within 140 characters)
-            </label>
-            <input type="text" class="form-control" id="title" name="title" max="140" required>
+<div class="container mt-5">
+    <div class="card border-primary">
+        <div class="card-header">
+            新しいTODOの追加
         </div>
-        <div class="form-group  mt-md-3">
-            <label for="deadline">
-                Deadline
-            </label>
-            <input type="datetime-local" id="deadline" name="deadline" step="600" required>
-        </div>
-        <button type="submit" class="btn btn-primary">
-            新規作成
-        </button>
-    </form>
-
+        <div class="card-body">
+            <form action="index.php" method="post">
+            <input type="hidden" name="mode" value="create">
+            <div class="form-group row">
+                <label for="title" class="col-sm-4 col-form-label">
+                    やること(140文字以内)
+                </label>
+                <div class="col-sm-8">
+                    <input type="textarea" class="form-control" id="title" name="title" max="140" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="deadline" class="col-sm-4 col-form-label">
+                    期限
+                </label>
+                <div class="col-sm-8">
+                    <input type="datetime-local" id="deadline" name="deadline" step="600" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-10">
+                    <button type="submit" class="btn btn-primary">
+                        新規作成
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
 
 <?= $script ?>
