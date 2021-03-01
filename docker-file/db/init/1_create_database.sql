8260e50bfcedefdf849b85d0150e272e7487e92a
山下永遠 <yamashita@laptop-8rine23.local>
2021-03-01 20:37:58 +0900

    added DB for Todo's-status

--- docker-file/db/init/1_create_database.sql
+++ docker-file/db/init/1_create_database.sql
@@ -1,10 +1,19 @@
+create table public.todo_statuses
+(
+	id serial not null
+		constraint todo_statuses_pk
+			primary key,
+	status text default 'unknown' not null
+);
+
 create table public.todos
 (
 	id serial not null
 		constraint todos_pk
 			primary key,
 	title text default '' not null,
-	status text default 'todo' not null,
+	status_id integer default 1 not null,
+	foreign key (status_id) references public.todo_statuses(id),
 	finished_at timestamp without time zone default '2999-12-31 23:59:59' not null,
 	created_at timestamp without time zone default now() not null,
 	updated_at timestamp without time zone default now() not null
--- docker-file/db/init/2_insert_dummy_data.sql
+++ docker-file/db/init/2_insert_dummy_data.sql
@@ -1,9 +1,17 @@
-INSERT INTO todos
-    (title, status, finished_at, created_at)
+INSERT INTO todo_statuses
+    (status)
 VALUES
-    ('test1', 'todo', '2021-04-01 10:00:00', '2021-03-22 10:00:00'),
-    ('test2', 'doing', '2021-04-02 11:00:00', '2021-03-21 10:00:00'),
-    ('test3', 'done', '2021-02-03 12:00:00', '2021-02-01 10:00:00'),
-    ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'todo', '2021-04-04 13:00', '2021-03-25 10:00:00'),
-    ('<script>alert("crack");</script>', 'todo', '2021-04-04 13:00:00', '2021-03-20 10:00:00')
+    ('todo'),
+    ('doing'),
+    ('done')
+;
+
+INSERT INTO todos
+    (title, status_id, finished_at, created_at)
+VALUES
+    ('test1', 1, '2021-04-01 10:00:00', '2021-03-22 10:00:00'),
+    ('test2', 2, '2021-04-02 11:00:00', '2021-03-21 10:00:00'),
+    ('test3', 3, '2021-02-03 12:00:00', '2021-02-01 10:00:00'),
+    ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 2, '2021-04-04 13:00', '2021-03-25 10:00:00'),
+    ('<script>alert("crack");</script>', 1, '2021-04-04 13:00:00', '2021-03-20 10:00:00')
 ;
